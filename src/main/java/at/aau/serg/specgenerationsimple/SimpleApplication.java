25cd65c475f758f2a2d86c35d4203e3bf9194668
Alexander Lercher <alexander.lercher@aau.at>
2026-09-10 13:21:29 +0200

    Cleanup project

--- src/main/java/at/aau/serg/specgenerationsimple/SimpleApplication.java
+++ src/main/java/at/aau/serg/specgenerationsimple/SimpleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpecGenerationSimpleApplication {
+public class SimpleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpecGenerationSimpleApplication.class, args);
+        SpringApplication.run(SimpleApplication.class, args);
     }
 
 }
